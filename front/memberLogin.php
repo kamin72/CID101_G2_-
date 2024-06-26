368e789b0d03c8fe73de064840f9f5d4dfee1ad0
kamin72 <a1616001@gmail.com>
2024-06-26 09:33:30 +0800

    add

--- front/memberLogin.php
+++ front/memberLogin.php
@@ -1,11 +1,11 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+// header("Access-Control-Allow-Headers: Content-Type, Authorization");
+// if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+//     http_response_code(200);
+//     exit();
+// }
 
 
 $memId = $_POST["account"];
