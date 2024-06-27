7221c1b70585b8049dc55effd7a329b83ee18041
kamin72 <a1616001@gmail.com>
2024-06-28 03:43:52 +0800

    add 綠界 files

---
+++ front/SDK_PHP-master/example/Payment/Aio/CreateCreditOrder.php
@@ -0,0 +1,30 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+use Ecpay\Sdk\Factories\Factory;
+use Ecpay\Sdk\Services\UrlService;
+
+require __DIR__ . '../../../../../../vendor/autoload.php';
+
+$factory = new Factory([
+    'hashKey' => 'pwFHCqoQZGmho4w6',
+    'hashIv' => 'EkRm7iFT261dpevs',
+]);
+$autoSubmitFormService = $factory->create('AutoSubmitFormWithCmvService');
+
+$input = [
+    'MerchantID' => '3002607',
+    'MerchantTradeNo' => 'Testkk' . time(),
+    'MerchantTradeDate' => date('Y/m/d H:i:s'),
+    'PaymentType' => 'aio',
+    'TotalAmount' => 100,
+    'TradeDesc' => UrlService::ecpayUrlEncode('cid101G2'),
+    'ItemName' => '2020 紅酒 100 TWD x 1',
+    'ChoosePayment' => 'Credit',
+    'EncryptType' => 1,
+
+    // 請參考 example/Payment/GetCheckoutResponse.php 範例開發
+    'ReturnURL' => 'http://localhost/CID101_G2_php/front/SDK_PHP-master/example/Payment/Aio/GetCheckoutResponse.php',
+];
+$action = 'https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5';
+
+echo $autoSubmitFormService->generate($input, $action);
