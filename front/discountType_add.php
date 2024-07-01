916370406dabebad1ef6d71eb7d2abb77bcc043e
purplekuo <ang840725@gmail.com>
2024-07-01 15:46:35 +0800

    優惠券種類 查詢/新增/修改/刪除

---
+++ front/discountType_add.php
@@ -0,0 +1,22 @@
+<?php
+header("Access-Control-Allow-Origin:*");
+$data = json_decode(file_get_contents("php://input"), true);
+
+try {
+
+    require_once("./connectDataBase.php");
+    $sql = "INSERT INTO discount_type(dis_name, dis_amount, dis_set_date) VALUES(:dis_name, :dis_amount, :dis_set_date)";
+    $disStmt = $pdo->prepare( $sql );
+    $disStmt->bindValue(":dis_name", $data["dis_name"]);
+    $disStmt->bindValue(":dis_amount", $data["dis_amount"]);
+    $disStmt->bindValue(":dis_set_date", $data["dis_set_date"]);
+    $disStmt->execute();
+    $dis = $data;
+
+    echo json_encode(["error" => false, "msg" => "新增成功", "discount" => $dis]);
+
+} catch (PDOException $e) {
+	$result = ["error" => true, "msg" => $e->getMessage()];
+	echo json_encode($result);
+}
+?>
---
+++ front/discountType_update.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: application/json');
+$data = json_decode(file_get_contents('php://input'), true);
+
+try {
+    require_once("./connectDataBase.php");
+    $sql = "UPDATE discount_type SET dis_name = :dis_name, dis_amount = :dis_amount, dis_set_date = :dis_set_date WHERE dis_serial = :dis_serial";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(":dis_name", $data["dis_name"]);
+    $stmt->bindValue(":dis_amount", $data["dis_amount"]);
+    $stmt->bindValue(":dis_set_date", $data["dis_set_date"]);
+    $stmt->bindValue(":dis_serial", $data["dis_serial"]);
+    $stmt->execute();
+
+    echo json_encode(["error" => false, "msg" => "修改成功"]);
+} catch (PDOException $e) {
+    echo json_encode(["error" => true, "msg" => $e->getMessage()]);
+}
+?>
