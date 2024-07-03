743fd006c295b5437a81bd12b2c523857d9e3b8b
mitaaaa803 <a0956880803@gmail.com>
2024-07-03 12:07:34 +0800

    優惠券

--- front/saveCoupon.php
+++ front/saveCoupon.php
@@ -1,42 +1,51 @@
 <?php
-// saveCoupon.php
-
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: http://localhost:5174'); // 設定允許的跨域來源
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
-
-// 启用错误报告
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
 try {
     // 連線 mysql
     require_once ("./connectDataBase.php");
-
+    // $pdo->beginTransaction();
+    // print_r($pdo);
     // 解析前端發來的 JSON 數據
     $input = json_decode(file_get_contents('php://input'), true);
+    // print_r($input);
 
     if (isset($input['memberInfo'])) {
         $memberInfo = $input['memberInfo'];
-        $memberId = intval($memberInfo[0]['id']);
-        $coupons = $memberInfo[0]['coupons'];
+        // $memberId = intval($memberInfo[0]['no']);
+        // $coupons = $memberInfo[0]=>'coupons';
 
-        // 準備 SQL 指令
-        $sql = "INSERT INTO discount_got (member_id, amount, date) VALUES (:memberId, :amount, :date)";
+    //     // 準備 SQL 指令
+        $sql = "INSERT INTO discount_got  (member_no,  dis_serial) VALUES (:memberId,  :serial)";
+        ;
         
-        // 編譯並執行 SQL 指令
+    //     // 編譯並執行 SQL 指令
         $stmt = $pdo->prepare($sql);
         
-        foreach ($coupons as $coupon) {
-            $stmt->execute([
-                ':memberId' => $memberId,
-                ':amount' => $coupon['amount'],
-                ':date' => $coupon['date']
-            ]);
-        }
+        $stmt->bindValue(":memberId", $input=>"no");
+        $stmt->bindValue(":serial", $input=>"dis_serial");
+
+        $stmt->execute();
+        // $dis_got_serial = $pdo->lastInsertId();
+        // $stmt=[
+        //     'dis_got_serial' => $dis_got_serial,
+        //     'member_no' => $memberId,
+        //     'dis_got_date' => $coupon['date'],
+        //     'dis_serial' => $coupon['serial'],
+        //     'dis_use_date' => $coupon['useDate']
+        // ]
+        // $pdo->commit(); 
+
+        // foreach ($coupons as $coupon) {
+        //     $stmt->execute([
+        //         ':memberId' => $memberId,
+        //         ':date' => $coupon['date'],
+        //         ':serial' => $coupon['serial'],
+        //         ':useDate' => $coupon['useDate'] ?? null
+        //     ]);
+        // }
 
         $result = ['success' => true];
+        // print_r($result);
         echo json_encode($result);
     } else {
         $result = ['success' => false, 'msg' => 'Invalid input'];
