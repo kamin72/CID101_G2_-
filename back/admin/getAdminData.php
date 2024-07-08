584dafc224388f69ac6ad590667daad73328060c
kamin72 <a1616001@gmail.com>
2024-07-09 01:27:02 +0800

    管理員上下架功能.php

--- back/admin/getAdminData.php
+++ back/admin/getAdminData.php
@@ -1,7 +1,7 @@
 <?php
 try {
     //連線mysql
-    require_once ("../../front/connectDataBase.php");
+    require_once("../../front/connectDataBase.php");
 
 
     $Sql = "SELECT * FROM admin ";
@@ -12,13 +12,10 @@ try {
         $result = ['error' => false, 'adminData' => $adminDataRow];
     } else {
         $result = ['error' => true, 'msg' => '查無相關資料', 'adminData' => $adminDataRow];
-
     }
-
 } catch (PDOException $e) {
     $msg = '錯誤原因:' . $e->getMessage() . "," . "錯誤行號:" . $e->getLine() . "," . "錯誤文件:" . $e->getFile();
     $result = ['error' => true, 'msg' => $msg];
 }
 
 echo json_encode($result);
-?>
